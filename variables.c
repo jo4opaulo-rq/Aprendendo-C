5ae91d2d49ba27f9c632687f1b1c6af7a381c89d
João Paulo <joaoroque317@gmail.com>
2021-10-29 17:38:55 -0300

    Primeiras aulas

---
+++ variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+  int a, b; 
+  float salary = 56.66;
+  char letter = 'g';
+
+  a = 8;
+  b = 67;
+  int c = a+b;
+  printf("%d \n", c);
+  printf("%f \n", salary);
+  printf("%c \n", letter);
+
+  return 0;
+}
